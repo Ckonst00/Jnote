127ab26ab29edb28546630112025c409a34eb35b
Aleksi Kaurila <aleksi@fedora>
2026-06-08 14:35:11 +0300

    Fix: Fonts get loaded correctly now

--- src/main/java/org/example/jnote/NoteApplication.java
+++ src/main/java/org/example/jnote/NoteApplication.java
@@ -12,7 +12,7 @@ public class NoteApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(NoteApplication.class.getResource("main.fxml"));
-        Font.loadFont(getClass().getResourceAsStream("/org/example/jnote/fonts/YourFont.ttf"), 14);
+        Font.loadFont(getClass().getResourceAsStream("/org/example/jnote/fonts/JavanesiaStylish-DYM6W.ttf"), 14);
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("JNOTE");
         stage.initStyle(StageStyle.UNDECORATED); // removes native title bar
